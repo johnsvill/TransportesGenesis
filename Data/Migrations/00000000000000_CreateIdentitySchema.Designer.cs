651e710871cc4d8058697a1326ab26f9caadc00f
David Florian <jdavidfla@gmail.com>
2026-04-25 13:35:58 -0600

    Resueltos conflictos y proyecto compila; pendientes ajustes de warnings

--- Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -5,11 +5,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
-<<<<<<< HEAD
-using TransportesGenesis.Data;
-=======
 using TransportesGenesis.Data.Context;
->>>>>>> dev_jonathan
 
 namespace TransportesGenesis.Data.Migrations
 {
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
-<<<<<<< HEAD
-using TransportesGenesis.Data;
-=======
 using TransportesGenesis.Data.Context;
->>>>>>> dev_jonathan
 
 public static class Startup
 {
-	public static WebApplication InicializarApp(string[] args)
-	{
+    public static WebApplication InicializarApp(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
 
         ConfigureServices(builder);
@@ -23,24 +19,17 @@ public static class Startup
     private static void ConfigureServices(WebApplicationBuilder builder)
     {
         // Add services to the container.
-<<<<<<< HEAD
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-=======
-        var connectionString = builder.Configuration.GetConnectionString("TransportesGenesisConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var connectionString = builder.Configuration.GetConnectionString("TransportesGenesisConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
 
->>>>>>> dev_jonathan
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ApplicationDbContext>();
-<<<<<<< HEAD
-=======
 
->>>>>>> dev_jonathan
         builder.Services.AddControllersWithViews();
     }
 
@@ -53,7 +42,7 @@ public static class Startup
         }
         else
         {
-            app.UseExceptionHandler("/Home/Error");           
+            app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
         }
 
@@ -67,6 +56,6 @@ public static class Startup
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();       
+        app.MapRazorPages();
     }
 }
